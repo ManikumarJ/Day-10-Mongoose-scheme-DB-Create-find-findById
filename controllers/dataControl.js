32e97c5efe2cbe46e77c63929c6bfe38f4c9efcc
ManikumarJ <manikumarj6@gmail.com>
2026-02-21 22:40:56 +0530

    Updated

--- controllers/dataControl.js
+++ controllers/dataControl.js
@@ -1,5 +1,20 @@
+import userModel from '../controllers/dataControl.js'
+
 export const createData = async (req, res) => {
 
-  console.log(req.body);
+  try {
 
+    const add = await userModel.create(req.body)
+
+    if(add){
+        res.status(201).json({msg:"Successfully Added"})
+    }else{
+
+        res.status(404).json({msg:"Soemthing error"})
+    }
+    
+    } 
+    catch (error) {
+     res.status(404).json({msg:"Soemthing error",error})
+    }
 };
